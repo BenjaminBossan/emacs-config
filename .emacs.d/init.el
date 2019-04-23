b593741f42550af7a35818cd9475d90b896b0168
BenjaminBossan <b.bossan@gmail.com>
2019-04-23 23:56:23 +0200

    Delete SBCL related init
    Breaks if SBCL not installed or something.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,28 +25,6 @@
 
 (byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
 
-;; sbcl lisp
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "/usr/bin/sbcl")
-
-;; paredit stuff
-(autoload 'enable-paredit-mode "paredit"
-  "Turn on pseudo-structural editing of Lisp code."
-  t)
-(add-hook 'emacs-lisp-mode-hook       'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           'enable-paredit-mode)
-
-;; Stop SLIME's REPL from grabbing DEL,
-;; which is annoying when backspacing over a '('
-(defun override-slime-repl-bindings-with-paredit ()
-  (define-key slime-repl-mode-map
-    (read-kbd-macro paredit-backward-delete-key)
-    nil))
-(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
-
 ;; ben's additions start
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'whitespace-mode)
